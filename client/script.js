2eb1243b566a096c9ffd1f114e039fa6992452b6
kenrick95 <kenrick95@gmail.com>
2017-04-23 14:48:57 +0800

    Visualize latencies in canvas
    Limited to 100 requests only

--- client/script.js
+++ client/script.js
@@ -1,7 +1,8 @@
 const onresize = () => {
   const canvas = document.querySelector('canvas')
-  canvas.height = window.innerHeight - 8
+  canvas.height = window.innerHeight
   canvas.width = window.innerWidth
+  draw()
 }
 
 const ping = async () => {
@@ -16,13 +17,43 @@ const ping = async () => {
   })
   const responseData = await response.json()
   responseData.localArrival = (new Date()).toISOString()
+  for (var [key, value] of Object.entries(responseData)) {
+    responseData[key] = new Date(value)
+  }
   return responseData
+}
+
+const latencies = []
+
+const draw = () => {
+  const canvas = document.querySelector('canvas')
+  const context = canvas.getContext('2d')
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  const barWidth = 10
+  context.fillStyle = 'black'
+  latencies.forEach((value, index) => {
+    context.rect(0 + index * barWidth, canvas.height - value, barWidth, value)
+    context.fill()
+  })
+}
+
+const delay = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 100)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   onresize()
-  const canvas = document.querySelector('canvas')
-  const context = canvas.getContext('2d')
-  console.log(await ping())
+  while (latencies.length < 100) {
+    const pingData = await ping()
+    const latency = ((pingData.localArrival - pingData.localDeparture) - (pingData.remoteDeparture - pingData.remoteArrival)) / 2
+    console.log(latency)
+    latencies.push(latency)
+    draw()
+    await delay()
+  }
 })
 window.addEventListener('resize', onresize)
